5021dc0ccb6edcb323626f97f3980bfd1f814f6f
Dylan <dmj08bot@gmail.com>
2026-06-21 00:38:07 +1000

    Detailed most of the contact page

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfiolio_website/widgets.dart';
+import 'package:web/web.dart' as web;
 
 class ContactPage extends StatefulWidget {
   const ContactPage({super.key});
@@ -8,15 +12,224 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController messagingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height,
+      child: Center(
+        child: Row(
+          spacing: 50,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 750,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 26, 26, 26),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Color.fromARGB(255, 36, 36, 36))
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+              child: Form(
+                child: Column(
+                  spacing: 20,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Contact Me',
+                      style: GoogleFonts.inter(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.5
+                      ),
+                    ),
+                    Row(
+                      spacing: 50,
+                      children: [
+                        Expanded(
+                          child: detailEditingBox(
+                            name: 'Name', 
+                            hint: 'Jane Doe',
+                            icon: Icons.person_outline,
+                            controller: nameController, 
+                          ),
+                        ),
+                        Expanded(
+                          child: detailEditingBox(
+                            name: 'Email', 
+                            hint: 'name@email.com', 
+                            icon: Icons.alternate_email,
+                            controller: emailController, 
+                          ),
+                        ),
+                      ],
+                    ),
+                    detailEditingBox(
+                      name: 'Message', 
+                      hint: 'Type your message here', 
+                      icon: Icons.message_outlined,
+                      controller: messagingController, 
+                      maxLines: 10
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              width: 350,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 26, 26, 26),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Color.fromARGB(255, 36, 36, 36))
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 8,),
+                  socialBox(
+                    name: 'GitHub',
+                    icon: 'github.svg',
+                    link: 'https://github.com/galaximaster'
+                  ),
+                  socialBox(
+                    name: 'Email',
+                    icon: 'email.svg',
+                    link: 'mailto:dmj08bot@gmail.com',
+                    linkFront: 'dmj08bot@gmail.com'
+                  ),
+                  socialBox(
+                    name: 'LinkedIn',
+                    icon: 'linkedIn.svg',
+                    link: 'https://www.linkedin.com/in/dylan-j-3a3637317',
+                    linkFront: 'https://www.linkedin.com/in/dylan-j'
+                  ),
+                  socialBox(
+                    name: 'Based In',
+                    icon: 'location.svg',
+                    link: 'Sydney, Aus',
+                    enabled: false
+                  ),
+                  SizedBox(height: 8,),
+                ],
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+
+  Padding socialBox({
+    required String name,
+    required String link,
+    String? linkFront, // to allow link and the link text to be different
+    required String icon,
+    bool enabled = true
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Row(
+        spacing: 15,
         children: [
-          Text('test')
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 20, 20, 20,),
+              borderRadius: BorderRadius.circular(10)
+            ),
+            padding: EdgeInsets.all(12),
+            child: SvgPicture.asset(
+              'icons/$icon',
+              colorFilter: ColorFilter.mode(Color.fromARGB(255, 146, 146, 146), BlendMode.srcIn),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 16
+                ),
+              ),
+              SelectionArea(
+                child: GestureDetector(
+                  onTap: enabled ? () => web.window.open(link) : null,
+                  child: Hoverable(
+                    builder: (isHovered) {
+                      return AnimatedDefaultTextStyle(
+                        duration: Duration(milliseconds: 200),
+                        curve: Curves.easeOutQuad,
+                        style: TextStyle(
+                          color: isHovered ? Colors.white60 : Colors.white54,
+                          decoration: isHovered ? TextDecoration.underline : null
+                        ),
+                        child: Text(linkFront ?? link),
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
+    );
+  }
+
+  Widget detailEditingBox({
+    required String name,
+    required String hint,
+    required IconData icon,
+    required TextEditingController controller,
+    int? maxLines
+  }) {
+    return Column(
+      spacing: 10,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          spacing: 10,
+          children: [
+            Icon(
+              icon,
+              size: 18,
+            ),
+            Text(name),
+          ],
+        ),
+        TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            hintText: hint,
+            isDense: true,
+            contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+            hintStyle: TextStyle(
+              color: Colors.white38,
+              letterSpacing: 2,
+              fontSize: 15
+            ),
+            filled: true,
+            fillColor: Color.fromARGB(255, 20, 20, 20),
+            hoverColor: Color.fromARGB(255, 20, 20, 20),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color.fromARGB(255, 49, 55, 63)),
+              borderRadius: BorderRadius.circular(15)
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.tealAccent),
+              borderRadius: BorderRadius.circular(15)
+            ),
+          ), 
+          maxLines: maxLines,
+        ),
+      ],
     );
   }
 }
